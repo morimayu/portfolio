09efef4cb996ccd206fc72235fbd1704d1565c5a
morimayu <89449041+morimayu@users.noreply.github.com>
2021-10-19 22:14:17 +0900

    ハンバーガー

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -1,2 +1,18 @@
 'use strict';
 
+function FixedAnime() {
+  var headerH = $('#gnavi-link').outerHeight(true);
+  var scroll = $(window).scrollTop();
+  if (scroll >= headerH){//headerの高さ以上になったら
+      $('#gnavi-link').addClass('fixed');//fixedというクラス名を付与
+    }else{//それ以外は
+      $('#gnavi-linki').removeClass('fixed');//fixedというクラス名を除去
+    }
+}
+
+$(function() {
+	$('.btn-gNav').on("click", function(){
+		$(this).toggleClass('open');
+		$('#gNav').toggleClass('open');
+	});
+});
